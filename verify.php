34fc76765d600ff14ddd720ae858657a2c148b2c
krittin-58 <krittin.j58@rsu.ac.th>
2017-08-16 21:19:31 +0700

    verify.php

---
+++ verify.php
@@ -0,0 +1,15 @@
+<?php
+$access_token = 'mgdv3EKByCK+cQoaMC91XwpjTsAFsp2G3DbVME0dqUcZJaCVt6UetZ0s1nsOpNypU7DGGBL8jtm4G1vBVRFkfZ1eLtyA/gJx3cy2WBQKqjrQbezRW6TZgilx1cZoqSosoilmyjlFBz5NLO838gLb3QdB04t89/1O/w1cDnyilFU=';
+
+$url = 'https://api.line.me/v1/oauth/verify';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
